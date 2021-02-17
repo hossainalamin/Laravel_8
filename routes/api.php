f8d9fe221b6f89f42c28f8d911b179ee609d9c94
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-02-17 21:03:40 +0600

    routing

---
+++ routes/api.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/users',function(){
+return "Hi users";
+});
+
+//required parameter
+Route::get("/username/{name}",function($name){
+    return "Hi ".$name;
+});
+
+//option parameter
+Route::get("/usernumber/{number?}",function($number=null){
+return "Phone : ".$number;
+});
+
+//Local Route constrains
+Route::get("/userid/{id?}",function($id = null){
+    return "Id id : ".$id;
+})->where('id','[0-9]+');
+
+//global constrains
+Route::get('/info/{info?}',function($info = null){
+return "Info is : ".$info;
+});
+
+//respond in multiple url
+Route::match(['get','post'],'/students',function(Request $req){
+return "Request Method is ".$req->method();
+});
+
+//respond in any url
+Route::any('/posts',function(Request $req){
+return "The request method is ".$req->method();
+});
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//rediection from about us page
+Route::get('/about',function(){
+//return view('about');
+return redirect ("/");
+});
+
+//another way of routing
+//Route::view('contact','contact');
+
+//passing data with routing with route contraints
+Route::get('/contact/{number}',function($number){
+    return view ('contact',["number"=>"$number"]);
+})->where('number','[0-9]+');
